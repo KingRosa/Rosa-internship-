9c25cdb30bf7cebf49f9144e9210177f88dbb016
KingRosa19 <annelieserosa@icloud.com>
2026-06-24 00:50:29 -0400

    update hot collections to use owl carousel and skeleton loading state

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,42 +1,116 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
+import Slider from "react-slick"; 
+import OwlCarousel from "react-owl-carousel";
+import "owl.carousel/dist/owl.carousel";
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
 
 const HotCollections = () => {
+  const [collections, setCollections] = useState([]);
+  const [loading, setLoading] = useState(true); 
+  useEffect(() => {
+    fetch('https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections')
+      .then((response) =>  {
+        if (!response.ok) {
+          throw new Error('Network response processed an error. Please try again later');
+        }
+        return response.json();
+      })
+      .then((data) => {        
+        setCollections(data); 
+        setLoading(false); 
+      })
+      .catch((error) => {
+        console.error('Error fetching the API:', error);
+        setLoading(false);
+      });
+  }, []); 
+
+   
+
+  const options = {
+    loop: true, 
+    margin: 10, 
+    nav: true,
+    dots: false, 
+    smartSpeed: 500, 
+    responsive: {
+      0: {
+        items: 1, 
+      },
+      600: {
+        items: 2, 
+      },
+      768: {
+        items: 3, 
+      },
+      1024: {
+        items: 4,
+      },
+    },
+  }
+  
+
   return (
     <section id="section-collections" className="no-bottom">
       <div className="container">
         <div className="row">
           <div className="col-lg-12">
-            <div className="text-center">
-              <h2>Hot Collections</h2>
-              <div className="small-border bg-color-2"></div>
-            </div>
-          </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-              <div className="nft_coll">
-                <div className="nft_wrap">
-                  <Link to="/item-details">
-                    <img src={nftImage} className="lazy img-fluid" alt="" />
-                  </Link>
-                </div>
-                <div className="nft_coll_pp">
-                  <Link to="/author">
-                    <img className="lazy pp-coll" src={AuthorImage} alt="" />
-                  </Link>
-                  <i className="fa fa-check"></i>
-                </div>
-                <div className="nft_coll_info">
-                  <Link to="/explore">
-                    <h4>Pinky Ocean</h4>
-                  </Link>
-                  <span>ERC-192</span>
-                </div>
+            {/* 3. Conditional Switch: If loading, map out the blank skeleton cards */}
+            {loading ? (
+              <div className="row">
+                {[1, 2, 3, 4].map((index) => (
+                  <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
+                    <div className="nft_coll" style={{ background: "#fff", border: "1px solid #eee", padding: "15px", borderRadius: "8px" }}>
+                      {/* NFT Image Area Placeholder */}
+                      <div className="skeleton-box" style={{ width: "100%", height: "200px", borderRadius: "8px" }}></div>
+                      
+                      {/* Author Avatar Circle Placeholder */}
+                      <div className="skeleton-box" style={{ width: "50px", height: "50px", borderRadius: "50%", margin: "-25px auto 0 auto", border: "2px solid #fff" }}></div>
+                      
+                      {/* Info Text Bars */}
+                      <div style={{ padding: "15px 0 0 0", textAlign: "center" }}>
+                        <div className="skeleton-box" style={{ width: "70%", height: "18px", margin: "0 auto 10px auto", borderRadius: "4px" }}></div>
+                        <div className="skeleton-box" style={{ width: "40%", height: "14px", margin: "0 auto", borderRadius: "4px" }}></div>
+                      </div>
+                    </div>
+                  </div>
+                ))}
               </div>
-            </div>
-          ))}
+            ) : (
+              collections.length > 0 && (
+                <OwlCarousel className="owl-theme" {...options}>
+                  {collections.map((item) => (
+                    <div key={item.id} className="nft-slider-item">
+                      <div className="nft_coll" style={{ margin: "0 10px" }}>
+                        <div className="nft_wrap">
+                          <Link to="/item-details">
+                            <img src={item.nftImage} className="lazy img-fluid" alt={item.title} />
+                          </Link>
+                        </div>
+                        <div className="nft_coll_pp">
+                          <Link to="/author">
+                            <img className="lazy pp-coll" src={item.authorImage} alt="" />
+                          </Link>
+                          <i className="fa fa-check"></i>
+                        </div>
+                        <div className="nft_coll_info">
+                          <Link to="/item-details">
+                            <h4>{item.title}</h4>
+                          </Link>
+                          <span>ERC-{item.code}</span>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+                </OwlCarousel>
+              )
+            )}
+          </div>
+
         </div>
       </div>
     </section>
