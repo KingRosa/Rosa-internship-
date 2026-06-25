d40698dea51d1bc2bd0efe3b4ba3d397f4116624
KingRosa19 <annelieserosa@icloud.com>
2026-06-24 20:21:28 -0400

    Item details functionality improved

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React, { useEffect } from "react"; // 1. Need to import useEffect
 import Home from "./pages/Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Explore from "./pages/Explore";
@@ -5,16 +6,26 @@ import Author from "./pages/Author";
 import ItemDetails from "./pages/ItemDetails";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
+import AOS from "aos"; 
+import "aos/dist/aos.css"; 
 
 function App() {
+ useEffect(() => {
+  AOS.init({
+    duration: 3000, // Make it very slow so you can see it
+    once: false,    // Set to false so it re-triggers when you scroll
+    mirror: true,   // Animates back and forth
+  });
+}, []);
   return (
     <Router>
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/explore" element={<Explore />} />
-<Route path="/author/:id" element={<Author />} />
-<Route path="/item-details/:nftId" element={<ItemDetails />} />      </Routes>
+        <Route path="/author/:id" element={<Author />} />
+        <Route path="/item-details/:nftId" element={<ItemDetails />} />
+      </Routes>
       <Footer />
     </Router>
   );
--- src/pages/ItemDetails.jsx
+++ src/pages/ItemDetails.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import axios from "axios";
+import AOS from "aos";
 
 const ItemDetails = () => {
   const { nftId } = useParams();
@@ -9,7 +10,7 @@ const ItemDetails = () => {
 
   useEffect(() => {
     window.scrollTo(0, 0);
-    
+
     axios
       .get(`https://us-central1-nft-cloud-functions.cloudfunctions.net/itemDetails?nftId=${nftId}`)
       .then((response) => {
@@ -22,6 +23,13 @@ const ItemDetails = () => {
       });
   }, [nftId]);
 
+  // This ensures AOS recognizes the elements once they are rendered
+  useEffect(() => {
+    if (!loading) {
+      AOS.refresh();
+    }
+  }, [loading]);
+
   if (loading) {
     return (
       <div className="container text-center" style={{ padding: "100px 0" }}>
@@ -33,71 +41,74 @@ const ItemDetails = () => {
   return (
     <div id="wrapper">
       <section id="section-item-details" className="no-bottom">
-        <div className="container">
-          <div className="row" style={{ paddingTop: "40px" }}>
-            <div className="col-md-6 text-center">
-              <img
-                src={item.nftImage}
-                className="img-fluid img-rounded mb-sm-30"
-                alt={item.title}
-              />
-            </div>
+        <div 
+          data-aos="fade-down"
+          data-aos-easing="linear"
+          data-aos-duration="1500"
+        >
+          <div className="container">
+            <div className="row" style={{ paddingTop: "40px" }}>
+              <div className="col-md-6 text-center">
+                <img
+                  src={item.nftImage}
+                  className="img-fluid img-rounded mb-sm-30"
+                  alt={item.title}
+                />
+              </div>
 
-            <div className="col-md-6">
-            <div className="item_details_info">
-  <h2>{item.title}</h2>
+              <div className="col-md-6">
+                <div className="item_details_info">
+                  <h2>{item.title}</h2>
 
-  {/* Top: Stats, Description, and Price */}
-  <div className="item_info_counts" style={{ margin: "20px 0" }}>
-    <div className="item_info_views" style={{ marginRight: "15px", display: "inline-block" }}>
-      <i className="fa fa-eye"></i> {item.views}
-    </div>
-    <div className="item_info_likes" style={{ display: "inline-block" }}>
-      <i className="fa fa-heart"></i> {item.likes}
-    </div>
-  </div>
+                  <div className="item_info_counts" style={{ margin: "20px 0" }}>
+                    <div className="item_info_views" style={{ marginRight: "15px", display: "inline-block" }}>
+                      <i className="fa fa-eye"></i> {item.views}
+                    </div>
+                    <div className="item_info_likes" style={{ display: "inline-block" }}>
+                      <i className="fa fa-heart"></i> {item.likes}
+                    </div>
+                  </div>
 
-  <p>{item.description}</p>
+                  <p>{item.description}</p>
 
-  <h6>Price</h6>
-  <div className="nft-item-price" style={{ marginBottom: "30px" }}>
-    <h3>{item.price} ETH</h3>
-  </div>
+                  <h6>Price</h6>
+                  <div className="nft-item-price" style={{ marginBottom: "30px" }}>
+                    <h3>{item.price} ETH</h3>
+                  </div>
 
-  {/* Bottom: Owner and Creator */}
-  <div className="item_author_wrap" style={{ display: "flex", borderTop: "1px solid #eee", paddingTop: "20px" }}>
-    
-    <div className="item_author" style={{ flex: 1, display: "flex", alignItems: "center" }}>
-      <div className="author_list_pp">
-        <Link to={`/author/${item.ownerId}`}>
-          <img className="lazy" src={item.ownerImage} alt={item.ownerName} />
-          <i className="fa fa-check"></i>
-        </Link>
-      </div>
-      <div className="author_list_info" style={{ marginLeft: "15px" }}>
-        <span>Owner</span><br />
-        <Link to={`/author/${item.ownerId}`}>{item.ownerName}</Link>
-      </div>
-    </div>
+                  <div className="item_author_wrap" style={{ display: "flex", borderTop: "1px solid #eee", paddingTop: "20px" }}>
+                    <div className="item_author" style={{ flex: 1, display: "flex", alignItems: "center" }}>
+                      <div className="author_list_pp">
+                        <Link to={`/author/${item.ownerId}`}>
+                          <img className="lazy" src={item.ownerImage} alt={item.ownerName} />
+                          <i className="fa fa-check"></i>
+                        </Link>
+                      </div>
+                      <div className="author_list_info" style={{ marginLeft: "15px" }}>
+                        <span>Owner</span><br />
+                        <Link to={`/author/${item.ownerId}`}>{item.ownerName}</Link>
+                      </div>
+                    </div>
 
-    <div className="item_author" style={{ flex: 1, display: "flex", alignItems: "center" }}>
-      <div className="author_list_pp">
-        <Link to={`/author/${item.creatorId}`}>
-          <img className="lazy" src={item.creatorImage} alt={item.creatorName} />
-          <i className="fa fa-check"></i>
-        </Link>
-      </div>
-      <div className="author_list_info" style={{ marginLeft: "15px" }}>
-        <span>Creator</span><br />
-        <Link to={`/author/${item.creatorId}`}>{item.creatorName}</Link>
-      </div>
-    </div>
-  </div>
+                    <div className="item_author" style={{ flex: 1, display: "flex", alignItems: "center" }}>
+                      <div className="author_list_pp">
+                        <Link to={`/author/${item.creatorId}`}>
+                          <img className="lazy" src={item.creatorImage} alt={item.creatorName} />
+                          <i className="fa fa-check"></i>
+                        </Link>
+                      </div>
+                      <div className="author_list_info" style={{ marginLeft: "15px" }}>
+                        <span>Creator</span><br />
+                        <Link to={`/author/${item.creatorId}`}>{item.creatorName}</Link>
+                      </div>
+                    </div>
+                  </div>
 
-  <Link to="/" className="btn-main" style={{ marginTop: "30px", display: "inline-block" }}>
-    Back to Marketplace
-  </Link>
-</div>
+                  <Link to="/" className="btn-main" style={{ marginTop: "30px", display: "inline-block" }}>
+                    Back to Marketplace
+                  </Link>
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -106,4 +117,4 @@ const ItemDetails = () => {
   );
 };
 
-export default ItemDetails;
+export default ItemDetails;;
